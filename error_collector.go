0930803cdb46388d8c6b89b180973b7a73c8e7f5
Frederik Ring <frederik.ring@gmail.com>
2017-07-15 20:41:52 +0200

    remove needless unwrapper interface

--- error_collector.go
+++ error_collector.go
@@ -11,22 +11,10 @@ import (
 // values while satisfying the common error interface
 type ErrorCollector []error
 
-type unwrapper interface {
-	unwrap() []error
-}
-
 // New returns a new nil ErrorCollector
 func New() ErrorCollector {
 	var ec ErrorCollector
 	return ec
-}
-
-func (ec ErrorCollector) unwrap() []error {
-	collection := []error{}
-	for _, err := range ec {
-		collection = append(collection, err)
-	}
-	return collection
 }
 
 // Collect adds a single error or another collector to
@@ -35,8 +23,8 @@ func (ec *ErrorCollector) Collect(err error) {
 	if err == nil {
 		return
 	}
-	if castCollector, ok := err.(unwrapper); ok {
-		*ec = append(*ec, castCollector.unwrap()...)
+	if castCollector, ok := err.(ErrorCollector); ok {
+		*ec = append(*ec, castCollector...)
 	} else {
 		*ec = append(*ec, err)
 	}
